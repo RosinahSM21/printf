f07e99b8f23a9e0010088b2c7eb9e8438e836d2e
Rirhandzu Novela <rirhandzumahansi@gmail.com>
2023-05-30 03:37:56 -0700

    get_size

--- get_size.c
+++ get_size.c
@@ -8,21 +8,27 @@
  */
 int get_size(const char *format, int *i)
 {
-        int a;
-        int sz;
+	int a;
+	int sz;
 
-        a = *i + 1;
-        sz = 0;
+	a = *i + 1;
+	sz = 0;
 
-        if (format[a] == 'l')
-                sz = S_LONG;
-        else if (format[a] == 'h')
-                sz = S_SHORT;
-
-        if (sz == 0)
-                *i = a - 1;
-        else
-                *i = a;
-
-        return (sz);
+	if (format[a] == 'l')
+	{
+		sz = S_LONG;
+	}
+	else if (format[a] == 'h')
+	{
+		sz = S_SHORT;
+	}
+	if (sz == 0)
+	{
+		*i = a - 1;
+	}
+	else
+	{
+		*i = a;
+	}
+	return (sz);
 }
